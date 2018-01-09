85e2e59c074fb90514568737ed6514beed5eef6b
jmpark <jmpark@nine-m.com>
2018-01-09 22:28:19 +0900

    first commit

---
+++ Assets/Scripts/RoomSwitcher.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomSwitcher : MonoBehaviour {
+
+	protected int _index = 0;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Assert(transform.childCount != 0);
+		setRoomIdx(0);
+	}
+	
+	public void nextRoom()
+	{
+		setRoomIdx(_index + 1);
+	}
+
+	public void prevRoom()
+	{
+		setRoomIdx(_index - 1);
+	}
+
+	protected void setRoomIdx(int idx)
+	{
+		_index = idx % transform.childCount;
+		if (_index < 0)
+			_index += transform.childCount;
+
+		for (int i=0; i<transform.childCount; ++i)
+		{
+			var room = transform.GetChild(i);
+			room.gameObject.SetActive(_index == i);
+		}
+	}
+}
---
+++ Assets/Scripts/Television.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class Television : MonoBehaviour {
+
+	[SerializeField]
+	Sprite background_image;
+
+	[SerializeField]
+	Sprite effect_image;
+
+	[SerializeField]
+	Sprite symbol_image;
+
+	SpriteRenderer _background;
+	SpriteRenderer _effect;
+	SpriteRenderer _symbol;
+
+	// Use this for initialization
+	void Start () {
+		_background = transform.Find("Background").GetComponent<SpriteRenderer>();
+		_effect = transform.Find("Effect").GetComponent<SpriteRenderer>();
+		_symbol = transform.Find("Symbol").GetComponent<SpriteRenderer>();
+
+		if (_background != null)
+			_background.sprite = background_image;
+		if (_effect != null)
+			_effect.sprite = effect_image;
+		if (_symbol != null)
+			_symbol.sprite = symbol_image;
+	}
+}
