1c7a10ef82a2b2b2e04138da01d81d1c6d1bc255
jmpark <jmpark@nine-m.com>
2018-01-13 16:46:02 +0900

    vote view

---
+++ Assets/Scripts/VoteDetailView.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoteDetailView : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+		if (GetComponent<Canvas>() != null)
+		{
+			// ui 컴포넌트인 경우 캔버스로 옮겨준다
+			var uiRoot = GameObject.FindGameObjectWithTag("RootCanvas");
+			transform.SetParent(uiRoot.transform);
+		}
+
+	}
+
+	public void OnAnswer(bool accept)
+	{
+		Debug.Log(accept.ToString());
+		GameObject.Destroy(gameObject);
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
