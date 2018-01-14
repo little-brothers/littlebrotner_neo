c2ca17f44787f5fa771c6b9f0310a1e5d68ae148
jmpark <jmpark@nine-m.com>
2018-01-14 18:39:59 +0900

    more simpler transition effect

--- Assets/Scripts/RoomSwitcher.cs
+++ Assets/Scripts/RoomSwitcher.cs
@@ -4,14 +4,12 @@ using UnityEngine;
 
 public class RoomSwitcher : MonoBehaviour {
 
-	[SerializeField]
-	SpriteRenderer transitionPanel;
-
 	protected int _index = 0;
 	protected bool _inTransition = false;
 
 	// Use this for initialization
 	void Start () {
+		GetComponent<SpriteRenderer>().sortingOrder = 1; // 항상 최상위에 표시됨
 		Debug.Assert(transform.childCount != 0);
 		setRoomIdx(0);
 	}
@@ -38,56 +36,30 @@ public class RoomSwitcher : MonoBehaviour {
 		StartCoroutine(fadeInOut(1.0f, _index));
 	}
 
-	void flipScene(int scene)
+	void flipSceneNow()
 	{
 		for (int i=0; i<transform.childCount; ++i)
 		{
 			var room = transform.GetChild(i);
-			room.gameObject.SetActive(scene == i);
+			room.gameObject.SetActive(_index == i);
 		}
 	}
 
 	IEnumerator fadeInOut(float duration, int scene)
 	{
 		_inTransition = true;
-		if (transitionPanel != null)
-		{
-			// lock the screen
-			transitionPanel.gameObject.SetActive(true);
 
-			float endTime = Time.time + duration;
-			float half = Time.time + duration/2;
-			bool changed = false;
-			var color = transitionPanel.color;
-			while (Time.time < endTime)
-			{
-				float progress = Time.time - half;
-				if (progress < 0)
-				{
-					progress = progress * -1;
-				}
-				else if (!changed)
-				{
-					changed = true;
-					flipScene(scene);
-				}
+		// lock the screen
+		GetComponent<SpriteRenderer>().enabled = true;
 
-				color.a = 1 - (progress / (duration/2));
-				transitionPanel.color = color;
+		var anim = GetComponent<Animation>();
+		anim.Play();
 
-				yield return null;
-			}
+		while(anim.isPlaying)
+			yield return null;
 
-			color.a = 0;
-			transitionPanel.color = color;
-
-			// unlock screen
-			transitionPanel.gameObject.SetActive(false);
-		}
-		else
-		{
-			flipScene(scene);
-		}
+		// unlock screen
+		GetComponent<SpriteRenderer>().enabled = false;
 
 		_inTransition = false;
 		yield break;
