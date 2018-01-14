ca23dadc73b7b52a663c5c7ec0dc1d1a860c69ab
jmpark <jmpark@nine-m.com>
2018-01-14 20:21:46 +0900

    dummy popups

---
+++ Assets/Scripts/TVDefault.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class TVDefault : MonoBehaviour, IPointerDownHandler {
+
+    void IPointerDownHandler.OnPointerDown(PointerEventData eventData)
+    {
+		GameObject.Destroy(gameObject);
+    }
+}
