164eb484b2bf9d27d8cc92548f483363cfdbedf8
KimDeokwon <64bit_decryption@naver.com>
2018-01-17 04:17:45 +0900

    Add TV06 script base

---
+++ Assets/Scripts/TV06.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TV06 : MonoBehaviour, IPointerDownHandler {
+    void IPointerDownHandler.OnPointerDown(PointerEventData eventData)
+    {
+        //throw new System.NotImplementedException();
+    }
+
+    private void Start () 
+	{
+		
+	}
+	
+	private void Update () 
+	{
+		
+	}
+}
