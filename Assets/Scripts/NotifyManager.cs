723e4e91687671a98ce73a6c4f72c17fd6cef43b
KimDeokwon <64bit_decryption@naver.com>
2018-01-17 04:41:05 +0900

    Add observer pattern

---
+++ Assets/Scripts/NotifyManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class NotifyManager {
+
+	private static List<ISubscribe> _subscribers;
+
+	static NotifyManager()
+	{
+		_subscribers = new List<ISubscribe>();
+	}
+
+	static void Subscribe(ISubscribe subscriber)
+	{
+		_subscribers.Add(subscriber);
+	}
+
+	static void UnSubscribe(ISubscribe subscriber)
+	{
+		_subscribers.Remove(subscriber);
+	}
+
+	/// <Summery>
+	/// values의 첫번째 값은 string형식의 event name을 넣을 것
+	/// 나머지 값은 자유롭게 사용
+	/// </Summery>
+	static void Notify(params object[] values)
+	{
+		object[] parameters = values;
+
+		foreach (ISubscribe subscriber in _subscribers)
+		{
+			subscriber.GetMessage(parameters);
+		}
+	}
+}
