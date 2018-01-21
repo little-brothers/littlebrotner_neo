fefcb2f3c6af12831d29c3adad68462a9a139fc6
jmpark <jmpark@nine-m.com>
2018-01-21 19:31:39 +0900

    disable arrow button when popup exists

---
+++ Assets/Scripts/RootCanvas.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RootCanvas : MonoBehaviour {
+
+	[SerializeField]
+	GameObject[] disableOnPopup;
+
+	void Update() {
+		bool on = transform.childCount == 0;
+
+		foreach (var go in disableOnPopup)
+			go.SetActive(on);
+	}
+}
