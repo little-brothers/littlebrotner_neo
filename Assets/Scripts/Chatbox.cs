c9fc4775fb205c0ca8b198f7f4f0978996390e61
jmpark <jmpark@nine-m.com>
2018-01-21 21:21:34 +0900

    chatbox event

---
+++ Assets/Scripts/Chatbox.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Chatbox : MonoBehaviour, IPointerClickHandler {
+	string _text;
+
+	[SerializeField]
+	Text textbox;
+
+	public static void Show(string content)
+	{
+		var prefab = Resources.Load<GameObject>("ChatBox");
+		var instance = GameObject.Instantiate(prefab).GetComponent<Chatbox>();
+		instance.textbox.text = content;
+		Utilities.SetUIParentFit(GameObject.FindWithTag("RootCanvas"), instance.gameObject);
+	}
+
+    void IPointerClickHandler.OnPointerClick(PointerEventData eventData)
+    {
+		GameObject.Destroy(gameObject);
+    }
+}
