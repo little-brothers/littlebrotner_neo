0218a96758ab33838d5f39650379b135de00ea9c
jmpark <jmpark@nine-m.com>
2018-01-21 16:37:10 +0900

    shop UI example

---
+++ Assets/Scripts/ShopListElement.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopListElement : MonoBehaviour {
+	[Serializable]
+	public struct Product {
+		public Texture2D icon;
+		public string name;
+		public int price;
+	}
+
+	Text _price;
+	Text _name;
+
+	void Awake()
+	{
+		_price = transform.Find("Price").GetComponent<Text>();
+		Debug.Assert(_price != null);
+
+		_name = transform.Find("Name").GetComponent<Text>();
+		Debug.Assert(_name != null);
+	}
+
+	public void Setup(Product product)
+	{
+		_price.text = product.price.ToString();
+		_name.text = product.name;
+	}
+}
--- Assets/Scripts/TV03.cs
+++ Assets/Scripts/TV03.cs
@@ -1,14 +1,36 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TV03 : MonoBehaviour {
+	Text _money;
+
+	[SerializeField]
+	ShopListElement.Product[] products;
+
+	[SerializeField]
+	VerticalLayoutGroup list;
 
 	void Start () {
-		
+		Utilities.SetUIParentFit(GameObject.FindGameObjectWithTag("RootCanvas"), gameObject);
+
+		_money = transform.Find("Money").GetComponent<Text>();
+		MyStatus.instance.money.OnUpdate += updateMoney;
+		updateMoney(MyStatus.instance.money);
+
+		var listElemTmpl = Resources.Load<GameObject>("ShopListElement");
+		foreach (var product in products)
+		{
+			var elem = GameObject.Instantiate(listElemTmpl, list.transform).GetComponent<ShopListElement>();
+			elem.Setup(product);
+		}
+	}
+
+	void updateMoney(int value)
+	{
+		_money.text = value.ToString();
 	}
 	
-	void Update () {
-		
-	}
 }
