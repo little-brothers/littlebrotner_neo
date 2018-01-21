832200c73308511255ae961a797cdd08d7c1e5d6
KimDeokwon <64bit_decryption@naver.com>
2018-01-21 21:07:36 +0900

    MyStatus change to global singleton script

--- Assets/Scripts/MyStatus.cs
+++ Assets/Scripts/MyStatus.cs
@@ -7,6 +7,16 @@ public class MyStatus : MonoBehaviour {
 	// singleton
 	static MyStatus _instance = null;
 
+	private void Awake()
+	{
+		if (_instance != null)
+		{
+			return;
+		}
+
+		DontDestroyOnLoad(gameObject);
+	}
+
 	public static MyStatus instance
 	{
 		get {
@@ -41,5 +51,6 @@ public class MyStatus : MonoBehaviour {
 	public DataUpdateNotifier<int> economy = new DataUpdateNotifier<int>();   // [공산주의 - 자본주의]
 	public DataUpdateNotifier<int> mechanic = new DataUpdateNotifier<int>();  // [신토불이 - 산업발전]
 	public DataUpdateNotifier<int> money = new DataUpdateNotifier<int>(); // 돈!
+	public DataUpdateNotifier<int> endingIndex = new DataUpdateNotifier<int>(); // 엔딩!
 	public DataUpdateNotifier<bool> isRobotAppear = new DataUpdateNotifier<bool>(); // 로봇 종족이 나타났는가
 }
