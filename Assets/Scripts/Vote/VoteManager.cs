c263e5f4dd2e8b236bcbcbfbc8b2059086340351
KimDeokwon <64bit_decryption@naver.com>
2018-01-24 23:07:52 +0900

    Add ending codition check code

--- Assets/Scripts/Vote/VoteManager.cs
+++ Assets/Scripts/Vote/VoteManager.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public static class VoteManager {
 
@@ -10,12 +11,16 @@ public static class VoteManager {
 	public static List<VoteData> voteDatas { get { return _voteDatas; } }
 	public static VoteData currentVote {get { return _voteDatas[_currentIndex]; } }
 
+	private static List<string> _endingCodition;
+
 	private static int _currentIndex = 0;
 	private static int _beforIndex = 0;
+	private static int _abstentionCount = 0;
 
 	public static bool Initialize(string fileName)
 	{
 		_voteDatas = new List<VoteData>();
+		_endingCodition = new List<string>();
 		
 		TextAsset csv = Resources.Load(fileName) as TextAsset;
 		if (csv == null)
@@ -41,8 +46,12 @@ public static class VoteManager {
 				//Index 13: 비동의 economy
 				//Index 14: 비동의 political
 				//Index 15: 비동의 mechanic
+				//Index 19: 엔딩조건
 				foreach(List<string> column in stringArray)
 				{
+					if (!column[19].Equals(""))
+						_endingCodition.Add(column[19]);
+
 					_voteDatas.Add(GenerateVoteData(column));
 				}
 			}
@@ -59,7 +68,7 @@ public static class VoteManager {
 		{
 			if (nextIndex.Contains("E"))
 			{
-				int endingIndex = Int32.Parse(nextIndex.Substring(1));
+				int endingIndex = Int32.Parse(nextIndex.Substring(1)) - 1;
 				data.endingIndexes.Enqueue(endingIndex);
 			}
 			else if (nextIndex.Contains("V-R"))
@@ -117,31 +126,41 @@ public static class VoteManager {
 		return data;
 	}
 
-	// 1 = Agree
-	// 0 = Abstention
-	// -1 = Disagree
 	public static void NextDay()
 	{
-		_beforIndex = _currentIndex;
+		_beforIndex = _currentIndex;				
 
 		string state = "?";
 		if (_voteDatas[_currentIndex].isAgree.Equals(-1))
 		{
 			state = "기권";
 			_currentIndex = _voteDatas[_currentIndex].abstention.nextVoteIndex;
-			Debug.Log(_voteDatas[_currentIndex].abstention.nextVoteIndex);
+			MyStatus.instance.economy.value += _voteDatas[_currentIndex].abstention.economy;
+			MyStatus.instance.political.value += _voteDatas[_currentIndex].abstention.political;
+			MyStatus.instance.mechanic.value += _voteDatas[_currentIndex].abstention.mechanic;
+			++_abstentionCount;
+			CheckEnding(_voteDatas[_beforIndex].abstention);
 		}
 		else if (_voteDatas[_currentIndex].isAgree.Equals(1))
 		{
 			state = "예";
 			_currentIndex = _voteDatas[_currentIndex].agree.nextVoteIndex;
+			MyStatus.instance.economy.value += _voteDatas[_currentIndex].agree.economy;
+			MyStatus.instance.political.value += _voteDatas[_currentIndex].agree.political;
+			MyStatus.instance.mechanic.value += _voteDatas[_currentIndex].agree.mechanic;
+			CheckEnding(_voteDatas[_beforIndex].agree);
 		}
 		else if (_voteDatas[_currentIndex].isAgree.Equals(0))
 		{
 			state = "아니오";
 			_currentIndex = _voteDatas[_currentIndex].disagree.nextVoteIndex;
-			
+			MyStatus.instance.economy.value += _voteDatas[_currentIndex].disagree.economy;
+			MyStatus.instance.political.value += _voteDatas[_currentIndex].disagree.political;
+			MyStatus.instance.mechanic.value += _voteDatas[_currentIndex].disagree.mechanic;
+			CheckEnding(_voteDatas[_beforIndex].disagree);
 		}
+
+
 		Debug.Log("이전 투표 결과: " + state + " | BI: " + _beforIndex + " | CI: " + _currentIndex);
 	}
 
@@ -150,5 +169,106 @@ public static class VoteManager {
 		VoteData temp = _voteDatas[_currentIndex];
 		temp.isAgree = agree;
 		_voteDatas[_currentIndex] = temp;
+	}
+
+	private static void CheckEnding(VoteDetailData data)
+	{
+		if (data.endingIndexes == null)
+		{
+			if (MyStatus.instance.health.value.Equals(0)) 
+			{
+				MyStatus.instance.endingIndex.value = 0;
+				SceneManager.LoadScene("EndingScene");
+			}
+			else if (_abstentionCount.Equals(3))
+			{
+				MyStatus.instance.endingIndex.value = 1;
+				SceneManager.LoadScene("EndingScene");
+			}
+			else if (MyStatus.instance.money.value >= 30)
+			{
+				MyStatus.instance.endingIndex.value = 19;
+				SceneManager.LoadScene("EndingScene");
+			}
+			return;
+		}
+
+		foreach (int index in data.endingIndexes)
+		{
+			string codition = _endingCodition[index];
+			if (IsEnding(codition))
+			{
+				MyStatus.instance.endingIndex.value = index;
+				SceneManager.LoadScene("EndingScene");
+			}
+		}
+	}
+
+	private static bool IsEnding(string codition)
+	{
+		string[] coditions = codition.Split('&');
+		bool returnValue = CheckCodition(coditions[0]);
+
+		if (coditions.Length.Equals(1))
+			return returnValue;
+
+		for (int i = 1; i < coditions.Length; ++i)
+		{
+			if (!returnValue)
+				break;
+			returnValue = returnValue && CheckCodition(coditions[i]);
+		}
+		return returnValue;
+	}
+
+	private static bool CheckCodition(string codition)
+	{
+		bool returnValue = false;
+		string[] value = codition.Split('-');
+		switch(value[0])
+		{
+			// case "Health": // 투표 결과에 영향을 받지 않는 엔딩은 바로 수행
+			// 	if (MyStatus.instance.health.value.Equals(Int32.Parse(value[1]))) 
+			// 	{
+			// 		MyStatus.instance.endingIndex.value = 0;
+			// 		SceneManager.LoadScene("EndingScene");
+			// 	}
+			// 	break;
+
+			// case "Abstention": // 투표 결과에 영향을 받지 않는 엔딩은 바로 수행
+			// 	if (_abstentionCount.Equals(Int32.Parse(value[1])))
+			// 	{
+			// 		MyStatus.instance.endingIndex.value = 1;
+			// 		SceneManager.LoadScene("EndingScene");
+			// 	}
+			// 	break;
+
+			// case "Money": // 투표 결과에 영향을 받지 않는 엔딩은 바로 수행
+			// 	if (MyStatus.instance.money.value.Equals(Int32.Parse(value[1])))
+			// 	{
+			// 		MyStatus.instance.endingIndex.value = 19;
+			// 		SceneManager.LoadScene("EndingScene");
+			// 	}
+			// 	break;
+
+			case "자유주의": // 나중에 자유주의, 사회주의등 가장 높은 수치 반환하는 함수 만들기
+				break;
+
+			case "사회주의":
+				break;
+
+			default:
+				int index = Int32.Parse(value[0].Substring(1)) - 1;
+				if (value[1].Equals("YES"))
+				{
+					returnValue = _voteDatas[index].agree.Equals(1) ? true : false;
+				}
+				else if (value[1].Equals("NO"))
+				{
+					returnValue = _voteDatas[index].agree.Equals(0) ? true : false;
+				}
+				break;
+		}
+		return returnValue;
 	}
 }
