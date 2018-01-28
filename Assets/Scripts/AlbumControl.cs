3770d9795e0cae193e9b04d2f106131a359baa2f
KimDeokwon <64bit_decryption@naver.com>
2018-01-28 19:44:23 +0900

    Add AlbumScene

---
+++ Assets/Scripts/AlbumControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlbumControl : MonoBehaviour {
+
+	[SerializeField]
+	private SpriteRenderer[] _screens;
+
+	[SerializeField]
+	private Sprite[] _endingThumbnails;
+
+	private void Start () 
+	{
+		for (int i = 1; i <= 20; ++i)
+		{
+			string key = "Ending" + i;
+			if (EncryptedPlayerPrefs.GetInt(key).Equals(1))
+				_screens[i - 1].sprite = _endingThumbnails[i - 1];
+		}
+	}
+}
