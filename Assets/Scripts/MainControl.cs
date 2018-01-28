e5edce6dcf98cd2ccb890723d6ccc6f200dd7525
KimDeokwon <64bit_decryption@naver.com>
2018-01-28 19:46:09 +0900

    Add MainScene

---
+++ Assets/Scripts/MainControl.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainControl : MonoBehaviour {
+
+	private void Awake()
+	{
+		Screen.SetResolution(1920, 1080, true, 0);
+		EncryptedPlayerPrefs.keys = new string[5];
+        EncryptedPlayerPrefs.keys[0] = "fgfkjldjds";
+        EncryptedPlayerPrefs.keys[1] = "tu849tu24";
+        EncryptedPlayerPrefs.keys[2] = "40kf0rk";
+        EncryptedPlayerPrefs.keys[3] = "jg05ji";
+        EncryptedPlayerPrefs.keys[4] = "erjeioj";
+
+		if (!EncryptedPlayerPrefs.HasKey("Ending1"))
+		{
+			for (int i = 1; i <= 20; i++)
+			{
+				EncryptedPlayerPrefs.SetInt("Ending" + i, 0);
+				EncryptedPlayerPrefs.SaveEncryption("Ending" + i, "int", "littlebrother");
+			}
+		}
+	}
+
+	public void PressStart()
+	{
+		SceneManager.LoadScene("VoteTestScene");
+	}
+
+	public void PressAlbum()
+	{
+		SceneManager.LoadScene("AlbumScene");
+	}
+
+	public void PressCredit()
+	{
+		SceneManager.LoadScene("CreditScene_Main");
+	}
+
+	public void PressEnd()
+	{
+		Application.Quit();
+	}
+}
