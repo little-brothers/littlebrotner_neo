584cd14252c025b5c357044c3c81f64fe6939f1f
jmpark <jmpark@nine-m.com>
2018-01-28 15:52:21 +0900

    show selected button

--- Assets/Scripts/VoteDetailView.cs
+++ Assets/Scripts/VoteDetailView.cs
@@ -8,9 +8,29 @@ public class VoteDetailView : MonoBehaviour {
 	[SerializeField]
 	Text questionText;
 
+	[SerializeField]
+	Button acceptButton;
+
+	[SerializeField]
+	Button declineButton;
+
 	// Use this for initialization
 	void Start () {
 		questionText.text = VoteManager.currentVote.voteTopic;
+		switch (VoteManager.currentVote.isAgree) {
+		case -1:
+			acceptButton.transform.Find("Selected").gameObject.SetActive(false);
+			declineButton.transform.Find("Selected").gameObject.SetActive(false);
+			break;
+
+		case 0: // decline
+			acceptButton.transform.Find("Selected").gameObject.SetActive(false);
+			break;
+
+		case 1:
+			declineButton.transform.Find("Selected").gameObject.SetActive(false);
+			break;
+		}
 	}
 
 	public void OnAnswer(bool accept)
