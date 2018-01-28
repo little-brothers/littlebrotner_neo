57a5dbd5f171ab3be17e1038fea2b93ca84bed16
jmpark <jmpark@nine-m.com>
2018-01-28 20:35:25 +0900

    tech tv scene

---
+++ Assets/Scripts/TV09.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TV09 : MonoBehaviour {
+
+	[SerializeField]
+	TechListElement.Technology[] technologies;
+
+	[SerializeField]
+	VerticalLayoutGroup list;
+
+	// Use this for initialization
+	void Start () {
+		var listElemTmpl = Resources.Load<GameObject>("TechListElement");
+		foreach (var tech in technologies)
+		{
+			var elem = GameObject.Instantiate(listElemTmpl, list.transform).GetComponent<TechListElement>();
+			elem.tech = tech;
+		}
+	}
+}
