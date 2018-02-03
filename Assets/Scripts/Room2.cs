a33f21136443d37d1b3e6839568cefed50c062a1
Jaemin <jmpark@nine-m.com>
2018-02-03 14:54:34 +0900

    turn on post processing only in room 2

---
+++ Assets/Scripts/Room2.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PostProcessing;
+
+public class Room2 : MonoBehaviour {
+
+	void OnEnable() {
+		// turn post processing on
+		Camera.main.GetComponent<PostProcessingBehaviour>().enabled = true;
+	}
+
+	void OnDisable() {
+		// turn post processing off
+		Camera.main.GetComponent<PostProcessingBehaviour>().enabled = false;
+	}
+}
