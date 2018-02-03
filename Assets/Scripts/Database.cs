0964bed404592e3d2295410c5378544281a32ccc
nullbus <ppparkje@gmail.com>
2018-02-03 10:25:50 +0900

    add item table

---
+++ Assets/Scripts/Database.cs
@@ -0,0 +1,83 @@
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEngine;
+
+[AttributeUsage(AttributeTargets.Class)]
+public class AssetPathAttribute : Attribute {
+	public AssetPathAttribute(string path) {
+		this.Path = path;
+	}
+
+	public string Path { get; set; }
+}
+
+public interface IDatabaseRow {
+	bool Parse(List<string> row);
+
+	int ID { get; }
+}
+
+public class Database<T> where T:IDatabaseRow, new() {
+	static Database<T> _instance = null;
+
+    Dictionary<int, T> _items = new Dictionary<int, T>();
+
+	public static Database<T> instance {
+		get {
+			if (_instance == null) {
+				AssetPathAttribute pathAttr = typeof(T).GetCustomAttributes(typeof(AssetPathAttribute), false)[0] as AssetPathAttribute;
+				Debug.Assert(pathAttr != null, "T in Database<T> must have [AssetPath] attribute!");
+
+				_instance = new Database<T>(pathAttr.Path);
+			}
+
+			return _instance;
+		}
+	}
+
+	public Database(string path)
+	{
+        TextAsset csv = Resources.Load(path) as TextAsset;
+        if (csv == null) {
+            Debug.Assert(false);
+            return;
+        }
+
+        using (var streamReader = new StreamReader(new MemoryStream(csv.bytes)))
+        {
+            using (var reader = new Mono.Csv.CsvFileReader(streamReader))
+            {
+                List<string> row = new List<string>(); // use as buffer
+                reader.ReadRow(row); // first row is header
+
+                // keep read content
+                while(reader.ReadRow(row)) {
+                    if (row[0] == "")
+                        break;
+
+                    T elem = new T();
+                    bool ret = elem.Parse(row);
+                    Debug.Assert(ret);
+
+                    _items.Add(elem.ID, elem);
+                }
+            }
+        }
+
+        Debug.Log(path + " loaded");
+	}
+
+    public T Find(int id) {
+        if (_items.ContainsKey(id))
+            return _items[id];
+
+        return default(T);
+    }
+
+    public List<T> ToList() {
+        return _items.Values.ToList();
+    }
+}
---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Currency {
+	Gold,
+	Electrocity,
+}
+
+[AssetPath("items")]
+public class Item : IDatabaseRow {
+	public int id { get; private set; }
+	public string name { get; private set; }
+	public string description { get; private set; }
+	public int price { get; private set; }
+	public Currency currency { get; private set; }
+	public bool installable { get; private set; } // 설치형인가? false라면 소모형
+	public string[] showConditions { get; private set; } // 상점 출현 조건
+
+    int IDatabaseRow.ID {
+        get { return this.id; }
+    }
+
+    bool IDatabaseRow.Parse(List<string> row)
+    {
+		Debug.Assert(row[0].StartsWith("I"));
+		if (!row[0].StartsWith("I"))
+			return false;
+
+		id = int.Parse(row[0].Substring(1));
+		name = row[2];
+		description = row[4];
+
+		var pricing = row[5].Split(':');
+		price = int.Parse(pricing[1]);
+		currency = pricing[0] == "E" ? Currency.Electrocity : Currency.Gold;
+
+		installable = row[6] == "설치형";
+		showConditions = new string[2]{row[8], row[9]};
+
+		return true;
+    }
+}
