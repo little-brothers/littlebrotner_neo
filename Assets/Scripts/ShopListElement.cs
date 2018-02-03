56250c063742af62753d61d0703d12bbefafa2f9
nullbus <ppparkje@gmail.com>
2018-02-03 11:28:56 +0900

    remove buy button then add highlight for ShopListElement

--- Assets/Scripts/ShopListElement.cs
+++ Assets/Scripts/ShopListElement.cs
@@ -5,54 +5,55 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class ShopListElement : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+public class ShopListElement : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler {
 	Item _product;
 	public Item product {
 		set {
 			_product = value;
 			_price.text = _product.price.ToString();
 			_name.text = _product.name;
-			updateBuyable(MyStatus.instance.money);
 		}
 	}
 
 	Text _price;
 	Text _name;
-	Button _buyButton;
 
 	void Awake()
 	{
 		_price = transform.Find("Price").GetComponent<Text>();
 		_name = transform.Find("Name").GetComponent<Text>();
-		_buyButton = transform.Find("BuyButton").GetComponent<Button>();
-
-		OnPointerExit(null);
-
-		MyStatus.instance.money.OnUpdate += updateBuyable;
-	}
-
-	void updateBuyable(int money)
-	{
-		_buyButton.interactable = money >= _product.price;
+		GetComponent<CanvasRenderer>().SetAlpha(0);
 	}
 
 	public void OnBuyButton()
 	{
-		ConfirmPopup.Setup(string.Format("Are you sure to buy '{0}'?", _product.name), () => {
-			MyStatus.instance.money.value -= _product.price;
-			// TODO 아이템 효과
-		});
+		if (MyStatus.instance.money >= _product.price) {
+			ConfirmPopup.Setup(string.Format("Are you sure to buy '{0}'?", _product.name), () => {
+				MyStatus.instance.money.value -= _product.price;
+				// TODO 아이템 효과
+			});
+		}
 	}
 
-    public void OnPointerExit(PointerEventData eventData)
-    {
-		_price.gameObject.SetActive(true);
-		_buyButton.gameObject.SetActive(false);
-    }
+	void IPointerExitHandler.OnPointerExit(PointerEventData eventData)
+	{
+		var renderer = GetComponent<CanvasRenderer>();
+		var color = renderer.GetColor();
+		color.a = 0;
+		renderer.SetColor(color);
+	}
 
-    public void OnPointerEnter(PointerEventData eventData)
-    {
-    	_price.gameObject.SetActive(false);
-    	_buyButton.gameObject.SetActive(true);
-    }
+	void IPointerEnterHandler.OnPointerEnter(PointerEventData eventData)
+	{
+		var renderer = GetComponent<CanvasRenderer>();
+		var color = renderer.GetColor();
+		color.a = 0.1f;
+		renderer.SetColor(color);
+	}
+
+	void IPointerClickHandler.OnPointerClick(PointerEventData eventData)
+	{
+		OnBuyButton();
+	}
+
 }
