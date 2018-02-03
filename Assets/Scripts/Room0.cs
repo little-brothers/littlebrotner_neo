3c76350bc49f7ce0031088e85623ec906fc43ce4
Jaemin <jmpark@nine-m.com>
2018-02-03 15:26:22 +0900

    disable move button at end if loop is disabled

--- Assets/Scripts/Room0.cs
+++ Assets/Scripts/Room0.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Room3 : MonoBehaviour {
+public class Room0 : MonoBehaviour {
 
 	[SerializeField]
 	Color normalColor;
--- Assets/Scripts/RoomSwitcher.cs
+++ Assets/Scripts/RoomSwitcher.cs
@@ -1,17 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RoomSwitcher : MonoBehaviour {
 
 	protected int _index = 0;
 	protected bool _inTransition = false;
 
+	[SerializeField]
+	int startIndex = 0;
+
+	[SerializeField]
+	bool loop = true;
+
+	[SerializeField]
+	Button leftButton;
+
+	[SerializeField]
+	Button rightButton;
+
 	// Use this for initialization
 	void Start () {
 		GetComponent<SpriteRenderer>().sortingOrder = 1; // 항상 최상위에 표시됨
 		Debug.Assert(transform.childCount != 0);
-		setRoomIdx(0);
+		setRoomIdx(startIndex);
 
 		// turn off all rooms
 		for (int i=0; i<transform.childCount; ++i)
@@ -19,23 +32,31 @@ public class RoomSwitcher : MonoBehaviour {
 			var room = transform.GetChild(i);
 			room.gameObject.SetActive(false);
 		}
+
+		leftButton.onClick.AddListener(() => {
+			setRoomIdx(_index-1);
+			updateButtonInteractable();
+		});
+
+		rightButton.onClick.AddListener(() => {
+			setRoomIdx(_index+1);
+			updateButtonInteractable();
+		});
+	}
+
+	void updateButtonInteractable()
+	{
+		leftButton.interactable = _index > 0;
+		rightButton.interactable = _index < transform.childCount-1;
 	}
 	
-	public void nextRoom()
-	{
-		setRoomIdx(_index + 1);
-		NotifyManager.Notify(EventNames.TurnOffTV);
-	}
-
-	public void prevRoom()
-	{
-		setRoomIdx(_index - 1);
-		NotifyManager.Notify(EventNames.TurnOffTV);
-	}
-
-	protected void setRoomIdx(int idx)
+	public void setRoomIdx(int idx)
 	{
 		if (_inTransition)
+			return;
+
+		// 루프 방지
+		if (!loop && (idx < 0 || idx >= transform.childCount))
 			return;
 
 		_index = idx % transform.childCount;
