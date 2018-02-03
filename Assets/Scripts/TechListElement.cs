906076914dfc14e80cee4188a8840c066501245e
nullbus <ppparkje@gmail.com>
2018-02-03 11:59:19 +0900

    Add TechList also to be ListElementBase

--- Assets/Scripts/TechListElement.cs
+++ Assets/Scripts/TechListElement.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class TechListElement : MonoBehaviour {
+public class TechListElement : ListElementBase {
 
 	[Serializable]
 	public struct Technology
