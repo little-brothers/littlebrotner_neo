cab3f7c1992c515f4a46c0e1cf496d3f680cff71
jmpark <jmpark@nine-m.com>
2018-02-10 17:45:44 +0900

    English version of news

--- Assets/Scripts/TV04.cs
+++ Assets/Scripts/TV04.cs
@@ -5,49 +5,29 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
+
 public class TV04 : MonoBehaviour, IPointerClickHandler {
-	struct News {
+
+	[AssetPath("news")]
+	struct News : IDatabaseRow
+	{
+		public int id;
 		public string article;
-	}
 
-	const string newsFile = "news";
+        int IDatabaseRow.ID { get { return id; } }
 
-	static List<News> news;
+        bool IDatabaseRow.Parse(List<string> row)
+        {
+			id = int.Parse(row[0].Substring(1));
+			article = row[2];
+
+			return true;
+        }
+    }
 
 	// Use this for initialization
 	void Start () {
-		if (news == null) {
-			// load once
-			TV04.news = new List<News>();
-			TextAsset csv = Resources.Load(newsFile) as TextAsset;
-			if (csv == null) {
-				Debug.Assert(false);
-				return;
-			}
-
-			using (var streamReader = new StreamReader(new MemoryStream(csv.bytes)))
-			{
-				using (var reader = new Mono.Csv.CsvFileReader(streamReader))
-				{
-					// 0: id (N1)
-					// 1: article
-					List<string> row = new List<string>();
-					reader.ReadRow(row); // first row is header
-
-					// keep read content
-					while(reader.ReadRow(row)) {
-						var news = new News();
-						news.article = row[1];
-
-						TV04.news.Add(news);
-					}
-				}
-			}
-
-			Debug.Log("news loaded");
-		}
-
-		var currentNews = news[VoteManager.currentVote.id-1];
+		var currentNews = Database<News>.instance.Find(VoteManager.currentVote.id);
 		transform.Find("Article").GetComponent<Text>().text = currentNews.article;
 	}
 
