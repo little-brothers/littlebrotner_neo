f79625be1b58009e16a78802d40e8606685f9f87
jmpark <jmpark@nine-m.com>
2018-02-11 18:09:56 +0900

    implement alert alarm

--- Assets/Scripts/Television.cs
+++ Assets/Scripts/Television.cs
@@ -59,6 +59,7 @@ public class Television : MonoBehaviour, IPointerClickHandler, IPointerEnterHand
 	EnergyManager _energyManager;
 	GameObject _overlay;
 	int _eventDay = 0; // 마지막으로 이벤트가 있었던 날짜
+	int _disasterDay = 0; // 마지막으로 재난이 일어난 날짜
 	Alarm _defaultAlarm;
 
 	//GameObject _noti;
@@ -72,21 +73,21 @@ public class Television : MonoBehaviour, IPointerClickHandler, IPointerEnterHand
 		}
 	}
 
-	// Use this for initialization
-	void Start () {
+	void Awake()
+	{
 		_background = GetComponent<SpriteRenderer>();
 		_effect = transform.Find("Effect").GetComponent<SpriteRenderer>();
 		_symbol = transform.Find("Symbol").GetComponent<SpriteRenderer>();
 		_energyManager = GameObject.FindObjectOfType<EnergyManager>();
 		_overlay = transform.Find("Overlay").gameObject;
 		_alarm = transform.Find("Alarm").GetComponent<SpriteRenderer>();
+	}
 
-		if (_background != null)
-			_background.sprite = background_image;
-		if (_effect != null)
-			_effect.sprite = effect_image;
-		if (_symbol != null)
-			_symbol.sprite = symbol_image;
+	// Use this for initialization
+	void Start () {
+		_background.sprite = background_image;
+		_effect.sprite = effect_image;
+		_symbol.sprite = symbol_image;
 
 		// 다음날 되면 본것들 저리
 		MyStatus.instance.AddSleepHook(ResetStatus);
@@ -165,6 +166,17 @@ public class Television : MonoBehaviour, IPointerClickHandler, IPointerEnterHand
 		// 브라운관 tv처럼 보이게 하려고 hue 주기적으로 업데이트
 		// deactivate되면 코루틴이 꺼지게 되므로 OnEnable에서 처리한다
 		StartCoroutine(UpdateHue());
+
+		if (_disasterDay == MyStatus.instance.day)
+		{
+			_alarm.gameObject.SetActive(true); // always show
+			SetAlarmType(AlarmAlert, false);
+			GetComponent<Animation>().Play("blink_alert", PlayMode.StopAll);
+		}
+		else
+		{
+			GetComponent<Animation>().Play("blink", PlayMode.StopAll);
+		}
 	}
 
 	void OnDisable()
@@ -172,6 +184,13 @@ public class Television : MonoBehaviour, IPointerClickHandler, IPointerEnterHand
 		_overlay.SetActive(false);
 	}
 
+	// invoked from blink_alert animation
+	public void ResetAlert()
+	{
+		GetComponent<Animation>().Play("blink");
+		SetAlarmType(_defaultAlarm, false);
+		UpdateAlarm(MyStatus.instance.day);
+	}
 
 	public void HoverSound(){
 
