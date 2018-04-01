66a02d6051da9e2e2314fa434c5dca2a2760f0df
jmpark <jmpark@nine-m.com>
2018-04-01 21:03:35 +0900

    installable item component

---
+++ Assets/Scripts/InstalledItem.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstalledItem : MonoBehaviour {
+
+	[SerializeField]
+	int itemID;
+
+	// Use this for initialization
+	void Start() {
+		gameObject.SetActive(false);
+		MyStatus.instance.inventory.OnUpdate += item => {
+			if (item.id == itemID)
+				gameObject.SetActive(true);
+		};
+	}
+}
