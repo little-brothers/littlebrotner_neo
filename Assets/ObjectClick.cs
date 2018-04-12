e830f8207efd829c5b686fdf53e22a98b5d2b7f1
yongjoo <contact.yongjoo@gmail.com>
2018-04-12 17:58:06 +0900

    mini game update

---
+++ Assets/ObjectClick.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectClick : MonoBehaviour {
+
+
+	void OnMouseDown()
+	{
+		Debug.Log ("CLick");
+		Destroy (gameObject);
+	}
+	// Use this for initialization
+
+
+	void Start () {
+
+
+		
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetMouseButton(0))
+			Debug.Log("Mouse down");
+		
+	}
+}
