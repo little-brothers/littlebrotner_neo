69681a93e499bf195258a220c51a4a231b8617b3
jmpark <jmpark@nine-m.com>
2018-04-15 15:36:24 +0900

    speech bubble on television 8

---
+++ Assets/Scripts/ScriptBubble.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ScriptBubble : MonoBehaviour, IPointerClickHandler {
+
+	[SerializeField]
+	public GameObject smallBubble;
+
+	[SerializeField]
+	public GameObject largeBubble;
+
+	void Start()
+	{
+		// smallBubble.GetComponent<Animation>().Play();
+		largeBubble.SetActive(false);
+	}
+
+	void toggleBubble()
+	{
+		if (smallBubble.activeSelf)
+		{
+			smallBubble.SetActive(false);
+			largeBubble.SetActive(true);
+		}
+	}
+
+	public void setText(string text)
+	{
+		largeBubble.transform.Find("Text").GetComponent<Text>().text = text;
+	}
+
+    void IPointerClickHandler.OnPointerClick(PointerEventData eventData)
+    {
+		toggleBubble();
+    }
+}
